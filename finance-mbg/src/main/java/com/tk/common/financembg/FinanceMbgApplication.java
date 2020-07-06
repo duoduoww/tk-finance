a1f5487ffe7baef0b33092cde346a884e9762896
kuang <1909864685@qq.com>
2020-07-06 17:24:09 +0800

    项目初始化

---
+++ finance-mbg/src/main/java/com/tk/common/financembg/FinanceMbgApplication.java
@@ -0,0 +1,13 @@
+package com.tk.common.financembg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceMbgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceMbgApplication.class, args);
+    }
+
+}
---
+++ finance-mbg/src/test/java/com/tk/common/financembg/FinanceMbgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tk.common.financembg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceMbgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
